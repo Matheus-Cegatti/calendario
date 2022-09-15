0ec8413ed6f16a3af5d4ffeba0aa203758a4a6de
Matheus-Cegatti <cegattimatheus@gmail.com>
2022-09-15 11:29:48 -0300

    definindo anos bissextos

--- script/script.js
+++ script/script.js
@@ -1,3 +1,7 @@
+let calendario = document.querySelector(".calendario");
+const meses = ['Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho', 'Julho', 'Agosto', 'Setembro', 'Outubro', 'Novembro', 'Dezembro'];
+
+
 var btnAlterar = document.querySelector(".modo-calendario")
 
 btnAlterar.addEventListener("click", ()=> {
@@ -5,3 +9,11 @@ btnAlterar.addEventListener("click", ()=> {
     document.querySelector("body").classList.toggle("estilo-clean")
 
 })
+
+anoBissexto = (ano) => {
+    return (ano % 4 === 0 && ano % 100 !== 0 && ano % 400 !== 0)/**pegando se o ano é multiplo de 4 e não de 100 */ || (ano % 100 === 0 && ano % 400 === 0)
+}
+
+gerarFevereiro = (ano) => {
+    return anoBissexto(ano) ? 29:28 //caso a condição de anoBissexto seja verdadeira retornará 29, caso seja falsa retorna 28
+}
