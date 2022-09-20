51289f27882c782e7ab930325e586c43b66ed56f
Matheus-Cegatti <cegattimatheus@gmail.com>
2022-09-20 11:32:21 -0300

    adicionando a classe selecionando ao dia clicado e removendo quando fecha o input

--- script/script.js
+++ script/script.js
@@ -3,7 +3,7 @@ const meses = ['Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho', 'Julh
 
 var btnAlterar = document.querySelector(".modo-calendario")
 
-btnAlterar.addEventListener("click", ()=> {
+btnAlterar.addEventListener("click", () => {
     document.querySelector("body").classList.toggle("dark");
     document.querySelector("body").classList.toggle("estilo-clean")
 
@@ -14,7 +14,7 @@ anoBissexto = (ano) => {
 }
 
 gerarFevereiro = (ano) => {
-    return anoBissexto(ano) ? 29:28 //caso a condição de anoBissexto seja verdadeira retornará 29, caso seja falsa retorna 28
+    return anoBissexto(ano) ? 29 : 28 //caso a condição de anoBissexto seja verdadeira retornará 29, caso seja falsa retorna 28
 }
 
 gerarCalendario = (mes, ano) => {
@@ -23,9 +23,9 @@ gerarCalendario = (mes, ano) => {
 
     let diasDoMes = [31, gerarFevereiro(ano), 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]; //colocando a quantidade de dias de cada mês e colocando a função que calcula o ano bissexto (dias de fevereiro).
 
-   diasDoCalendario.innerHTML = ''
+    diasDoCalendario.innerHTML = ''
 
-    
+
     console.log(dataAtual = new Date());
 
     let mesAtual = `${meses[mes]}`;
@@ -35,17 +35,17 @@ gerarCalendario = (mes, ano) => {
 
     let primeiroDia = new Date(ano, mes, 1); //define o dia 1 do mes (ele recebera a data atual no if com dataAtual.getDate())
 
-    for(let contador = 0; contador <= diasDoMes[mes] + primeiroDia.getDay()- 1; contador++) {
+    for (let contador = 0; contador <= diasDoMes[mes] + primeiroDia.getDay() - 1; contador++) {
         let dia = document.createElement("div");
-        if(contador>= primeiroDia.getDay()) {
+        if (contador >= primeiroDia.getDay()) {
             dia.classList.add('dias-numero-hover')
-            dia.innerHTML = contador - primeiroDia.getDay() +1 //define o total de dia de cada mês e retorna o numero
+            dia.innerHTML = contador - primeiroDia.getDay() + 1 //define o total de dia de cada mês e retorna o numero
             // dia.innerHTML += `<span></span>
             // <span></span>
             // <span></span>
             // <span></span>`
-            console.log(primeiroDia.getDay() +1);
-            if(contador - primeiroDia.getDay() +1 === dataAtual.getDate() && ano === dataAtual.getFullYear() && mes === dataAtual.getMonth()){
+            console.log(primeiroDia.getDay() + 1);
+            if (contador - primeiroDia.getDay() + 1 === dataAtual.getDate() && ano === dataAtual.getFullYear() && mes === dataAtual.getMonth()) {
                 dia.classList.add("data-atual")
                 console.log(contador);
             } //encontrando o dia corrente e adicionando as propriedades da classe de data-atual
@@ -60,7 +60,7 @@ gerarCalendario = (mes, ano) => {
 
 let listaMes = calendario.querySelector(".lista-meses")
 
-meses.forEach((evento, index)=> { //para cada mes ele recebe o nome (evento) e um numero (index), busco na lista de meses declarada no começo
+meses.forEach((evento, index) => { //para cada mes ele recebe o nome (evento) e um numero (index), busco na lista de meses declarada no começo
     let mes = document.createElement('div')
     mes.innerHTML = `<div data-mes="${index}">${evento}<div>`
     mes.querySelector('div').onclick = () => {
@@ -69,26 +69,26 @@ meses.forEach((evento, index)=> { //para cada mes ele recebe o nome (evento) e u
         gerarCalendario(mesAtual.value, anoAtual.value)
         // console.log(listaMes);
     }
- listaMes.appendChild(mes)
+    listaMes.appendChild(mes)
 })
 
 let selecionaMes = calendario.querySelector(".seleciona-mes");
 
-    selecionaMes.onclick = () => {
+selecionaMes.onclick = () => {
     listaMes.classList.add("show")
     // console.log("clicou");
 }
 
 let mudaMes = calendario.querySelector(".btn-mes--atual");
-    
-mudaMes.onclick= () => {
+
+mudaMes.onclick = () => {
     console.log("clicou");
     gerarCalendario(dataAtual.getMonth(), dataAtual.getFullYear(anoAtual))
 }
 
 let dataAtual = new Date()
-let mesAtual = {value: dataAtual.getMonth()}
-let anoAtual = {value: dataAtual.getFullYear()}
+let mesAtual = { value: dataAtual.getMonth() }
+let anoAtual = { value: dataAtual.getFullYear() }
 
 gerarCalendario(mesAtual.value, anoAtual.value)
 
@@ -105,24 +105,43 @@ document.querySelector("#proximo-ano").onclick = () => {
 // textoDia.appendChild(recado)
 
 
+// btnFechar.onclick = () => {
+//     console.log("botao");
+//     textoDia.classList.remove("aparece");
+//     btnFechar.classList.remove("aparece");
 
-let btnFechar = document.querySelector(".btn-fechar");
-btnFechar.onclick = () => {
-    console.log("botao");
-    textoDia.classList.remove("aparece")
-    btnFechar.classList.remove("aparece");
-}
+// }
+
+
 let textoDia = document.querySelector(".texto-dia")
 let recado = document.createElement('div')
 recado.innerHTML = `<div data-recado></div>`
 let todosDias = document.querySelectorAll(".dias-numero-hover");
 
-for(let i = 0; i < todosDias.length; i++) {
+for (let i = 0; i < todosDias.length; i++) {
     let diasSemana = todosDias[i];
 
-    diasSemana.onclick = function(){
-        console.log("Click dia");
-        textoDia.classList.add("aparece")
-    btnFechar.classList.add("aparece") 
+    diasSemana.onclick = function () {
+        // console.log("Click dia");
+        textoDia.classList.add("aparece");
+        btnFechar.classList.add("aparece");
+        diasSemana.classList.add("selecionado");
+        console.log(diasSemana);
+        btnFechar.onclick = () => {
+            textoDia.classList.remove("aparece");
+            btnFechar.classList.remove("aparece");
+            diasSemana.classList.remove("selecionado")
+        }
+        
     }
+
+    let btnFechar = document.querySelector(".btn-fechar");
+
+
 }
+
+
+
+
+
+
